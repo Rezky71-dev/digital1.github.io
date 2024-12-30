8cee499d406263b811ded92dec132284cc44566a
Rezky71-dev <rezkyanugrah93@gmail.com>
2024-12-30 09:31:22 +0700

    Add files via upload

---
+++ cees/index3.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+
+<!-- HTML Meta Tags -->
+<title>The Wedding of Aini & Rezky</title>
+<meta name="description" content="Kamis, 26 September 2024 - STITACO CAFE">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://airezwedding.my.id">
+<meta property="og:type" content="website">
+<meta property="og:title" content="The Wedding of Aini & Rezky">
+<meta property="og:description" content="Kamis, 26 September 2024 - STITACO CAFE">
+<!--
+  You can generate this image URL dynamically: https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/{site_text}/{title_text}/{image_url}/og.png
+  Replace the variables in the brackets with your own values and use this URL in the image tag below this comment. Ensure values are URL encoded.
+  For more information, read: https://www.opengraph.xyz/blog/how-to-implement-dynamic-open-graph-images
+-->
+<meta property="og:image" content="https://airezwedding.my.id/thumbnails/thumb.jpg">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="airezwedding.my.id">
+<meta property="twitter:url" content="https://airezwedding.my.id">
+<meta name="twitter:title" content="The Wedding of Aini & Rezky">
+<meta name="twitter:description" content="Kamis, 26 September 2024 - STITACO CAFE">
+<!--
+  You can generate this image URL dynamically: https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/{site_text}/{title_text}/{image_url}/og.png
+  Replace the variables in the brackets with your own values and use this URL in the image tag below this comment. Ensure values are URL encoded.
+  For more information, read: https://www.opengraph.xyz/blog/how-to-implement-dynamic-open-graph-images
+-->
+<meta name="twitter:image" content="https://airezwedding.my.id/thumbnails/thumb.jpg">
+
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->
+
+<link rel="shortcut icon" href="img/Text-Section/7.png" type="image/x-icon" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+<!-- REQUEST FONT -->
+<link href="https://fonts.cdnfonts.com/css/alegreya-sans" rel="stylesheet" />
+
+<link href="https://fonts.cdnfonts.com/css/alegreya" rel="stylesheet" />
+
+<link href="https://fonts.cdnfonts.com/css/haigrast-serif-deco-personal" rel="stylesheet" />
+
+<link href="https://fonts.cdnfonts.com/css/dancing-script" rel="stylesheet" />
+
+<link href="https://fonts.cdnfonts.com/css/bellefair" rel="stylesheet" />
+
+<link href="https://fonts.cdnfonts.com/css/haigrast-serif-deco-personal" rel="stylesheet" />
+<link href="https://fonts.cdnfonts.com/css/haigrast-script-personal-use" rel="stylesheet" />
+<!-- REQUEST FONT END -->
+<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Yellowtail&display=swap" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Tangerine:wght@400;700&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet" type="text/css" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Sacramento&family=Work+Sans:wght@100;300;400;600;700&display=swap"
+    rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
+
+<!-- simplyCountdown -->
+<link rel="stylesheet" href="countdown/simplyCountdown.theme.default.css" />
+<script src="countdown/simplyCountdown.min.js"></script>
+
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+<style>
+@font-face
+
+/*perintah untuk memanggil font eksternal*/
+    {
+    font-family: "Katalina";
+    /*memberikan nama bebas untuk font*/
+    src: url("font/Katalina.ttf");
+    /*memanggil file font eksternalnya di folder nexa*/
+}
+
+@font-face
+
+/*perintah untuk memanggil font eksternal*/
+    {
+    font-family: "Haigrast-serif";
+    /*memberikan nama bebas untuk font*/
+    src: url("font/Haigrast-Serif.ttf");
+    /*memanggil file font eksternalnya di folder nexa*/
+}
+
+@font-face
+
+/*perintah untuk memanggil font eksternal*/
+    {
+    font-family: "Eyesome";
+    /*memberikan nama bebas untuk font*/
+    src: url("font/Eyesome-Script.otf");
+    /*memanggil file font eksternalnya di folder nexa*/
+}
+
+@font-face
+
+/*perintah untuk memanggil font eksternal*/
+    {
+    font-family: "Haigrast-aja";
+    /*memberikan nama bebas untuk font*/
+    src: url("font/Haigrast.ttf");
+    /*memanggil file font eksternalnya di folder nexa*/
+}
+</style>
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+<link rel="stylesheet" href="gaya.css" />
+<link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<link href="js/lightboxed/lightboxed.css" rel="stylesheet" />
+<script src="js/lightboxed/lightboxed.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
